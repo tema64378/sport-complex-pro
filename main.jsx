84613a1540c07a4d33af92e65ff2df4978dced01
«Artem» <tema643789@gmail.com>
2026-02-03 21:34:54 +0300

    Fix app import case

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
